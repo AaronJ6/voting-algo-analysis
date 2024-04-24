e8d45d0989cb31f196dd616d93d584804b020ebe
AaronJ6 <aaronjoseph2002@gmail.com>
2024-04-24 18:57:20 +0530

    Graph label changes

--- pages/Results.py
+++ pages/Results.py
@@ -3,7 +3,6 @@ import pandas as pd
 import os
 from components.nav_page import nav_page
 import json
-
 st.set_page_config(initial_sidebar_state="collapsed")
 
 def clear_results_history():
@@ -61,42 +60,44 @@ else:
     chart_data =[]
     for elem in data:
         chart_data.append({
-            "n":elem["num_voters"],
-            "m":elem["num_candidates"],
-            "x":elem["group_size"],
-            "time":float(elem["time"][:-3]),
-            "memory":float(elem["memory"][:-3]),
-            "total_ops":elem["total_ops"]
+            "No_of_Voters":elem["num_voters"],
+            "No_of_Candidates":elem["num_candidates"],
+            "Group_Size":elem["group_size"],
+            "Time":float(elem["time"][:-3]),
+            "Memory":float(elem["memory"][:-3]),
+            "Total_Operations":elem["total_ops"]
         })
-    st.subheader("Variation with n")
+    st.subheader("Variation with No_of_Voters")
        #elements all values where  m,x is the same as the current results
-    n_data = [elem for elem in chart_data if elem["m"]==results["num_candidates"] and elem["x"]==results["group_size"]]
+    n_data = [elem for elem in chart_data if elem["No_of_Candidates"]==results["num_candidates"] and elem["Group_Size"]==results["group_size"]]
     if len(n_data)>1:
         df = pd.DataFrame(n_data)
-        st.line_chart(df[["n","time"]].set_index("n"))
-        st.line_chart(df[["n","memory"]].set_index("n"))
-        st.line_chart(df[["n","total_ops"]].set_index("n"))
+        st.line_chart(data = df[["No_of_Voters","Time"]].set_index("No_of_Voters"),y="Time")
+        st.line_chart(data = df[["No_of_Voters","Memory"]].set_index("No_of_Voters"),y="Memory")
+        st.line_chart(data = df[["No_of_Voters","Total_Operations"]].set_index("No_of_Voters"),y="Total_Operations")
     else:
         st.warning("Not enough data to plot graphs for variation with n ,vary n for the same m and x you entered now to see the graphs")
-    st.subheader("Variation with m")
+
+    st.subheader("Variation with No_of_Candidates")
     #flter out the data with same n and x
-    m_data = [elem for elem in chart_data if elem["n"]==results["num_voters"] and elem["x"]==results["group_size"]]
+    m_data = [elem for elem in chart_data if elem["No_of_Voters"]==results["num_voters"] and elem["Group_Size"]==results["group_size"]]
     
     if len(m_data)>1:
         df = pd.DataFrame(m_data)
-        st.line_chart(df[["m","time"]].set_index("m"))
-        st.line_chart(df[["m","memory"]].set_index("m"))
-        st.line_chart(df[["m","total_ops"]].set_index("m"))
+        st.line_chart(df[["No_of_Candidates","Time"]].set_index("No_of_Candidates"), y="Time")
+        st.line_chart(df[["No_of_Candidates","Memory"]].set_index("No_of_Candidates"), y="Memory")
+        st.line_chart(df[["No_of_Candidates","Total_Operations"]].set_index("No_of_Candidates"), y="Total_Operations")
     else:
         st.warning("Not enough data to plot graphs for variation with m ,vary m for the same n and x you entered now to see the graphs")
-    st.subheader("Variation with x")
+
+    st.subheader("Variation with Group_Size")
     #flter out the data with same n and m
-    x_data = [elem for elem in chart_data if elem["n"]==results["num_voters"] and elem["m"]==results["num_candidates"]]
+    x_data = [elem for elem in chart_data if elem["No_of_Voters"]==results["num_voters"] and elem["No_of_Candidates"]==results["num_candidates"]]
     if len(x_data)>1:
         df = pd.DataFrame(x_data)
-        st.line_chart(df[["x","time"]].set_index("x"))
-        st.line_chart(df[["x","memory"]].set_index("x"))
-        st.line_chart(df[["x","total_ops"]].set_index("x"))
+        st.line_chart(df[["Group_Size","Time"]].set_index("Group_Size"), y="Time")
+        st.line_chart(df[["Group_Size","Memory"]].set_index("Group_Size"), y="Memory")
+        st.line_chart(df[["Group_Size","Total_Operations"]].set_index("Group_Size"), y="Total_Operations")
     else:
         st.warning("Not enough data to plot graphs for variation with x ,vary x for the same n and m you entered now to see the graphs")
 
